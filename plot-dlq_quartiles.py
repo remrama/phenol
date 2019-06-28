15f5fa5028fd5ef127e2d32cabc61e6d1b12727a
remrama <mallett.remy@gmail.com>
2019-06-28 17:07:52 +0200

    back to vertical (one axis)

--- plot-dlq_quartiles.py
+++ plot-dlq_quartiles.py
@@ -35,32 +35,31 @@ plot_data_all = df[DLQ_COLS].values
 plot_data_lim = df.loc[df['DLQ:1']>1,DLQ_COLS].values
 
 # open figure
-fig, ax = plt.subplots(figsize=(2*len(DLQ_COLS),6))
+fig, axes = plt.subplots(2,1,figsize=(2*len(DLQ_COLS),12))
 
 # loop over two datasets
-for i, data in enumerate([plot_data_all,plot_data_lim]):
-
-    xvals = pd.np.arange(len(DLQ_COLS))
-    xvals = xvals-.2 if i==0 else xvals+.2
-    facecolor = 'white' if i==0 else 'silver'
+for i, (ax,data) in enumerate(zip(axes,[plot_data_lim,plot_data_all])):
+   
     # boxplot
-    ax.boxplot(data,widths=.4,positions=xvals,
+    ax.boxplot(data,widths=.4,#positions=pd.np.arange(.5,10.5),
                patch_artist=True,showbox=True,showfliers=True,
-               boxprops={'facecolor':facecolor},
+               boxprops={'facecolor':'gainsboro'},
                medianprops={'color':'red'})
 
-# aesthetics
-ax.set_xlim(-1,len(DLQ_COLS)+1)
-ax.set_xticks(range(len(DLQ_COLS)))
-ax.set_xticklabels(DLQ_COLS,rotation=25)
-ax.set_xlabel('DLQ probe')
-ax.set_ylabel('I was aware that I was dreaming')
-ax.set_yticks([1,2,3,4,5])
-ax.set_yticklabels(list(myplt.DLQ_STRINGS.values()),rotation=25)
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.grid(True,axis='y',which='major',linestyle='--',
-        linewidth=.25,color='k',alpha=1)
+    # aesthetics
+    ax.set_yticks([1,2,3,4,5])
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.grid(True,axis='y',which='major',linestyle='--',
+            linewidth=.25,color='k',alpha=1)
+    if i==0:
+        ax.set_ylabel('I was aware that I was dreaming')
+        ax.set_yticklabels(list(myplt.DLQ_STRINGS.values()),rotation=25)
+        ax.set_xticklabels([])
+    elif i==1:
+        ax.set_xlabel('DLQ probe')
+        ax.set_xticklabels(DLQ_COLS,rotation=25)
+        ax.set_yticklabels([])
 
 plt.tight_layout()
 
