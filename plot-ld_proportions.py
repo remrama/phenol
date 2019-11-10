b879126284af5604dbb26d75c25570ff8c893016
remrama <mallett.remy@gmail.com>
2019-11-10 14:11:44 -0600

    stats on induction success

--- plot-ld_proportions.py
+++ plot-ld_proportions.py
@@ -2,10 +2,14 @@
 Plot the proportion of lucid dreams across
 all subjects and sessions, at different
 DLQ1 cutoffs/criteria.
-"""
 
+Also save data and stats dataframes.
+"""
 from os import path
 import pandas as pd
+
+from itertools import combinations
+from statsmodels.stats.proportion import proportions_ztest
 
 import matplotlib; matplotlib.use('Qt5Agg') # python3 bug
 import matplotlib.pyplot as plt; plt.ion()
@@ -19,62 +23,108 @@ infname = path.join(resdir,'dlq1-frequency.tsv')
 
 ########  load and manipulate data  #########
 
-df = pd.read_csv(infname,index_col='subj',sep='\t')
+data = pd.read_csv(infname,index_col='subj',sep='\t')
 
-# get the proportion of LDs at each cutoff
-cumsum_cols = pd.np.roll(df.columns.sort_values(ascending=False),-1).tolist()
-cumsum_df = df[cumsum_cols].cumsum(axis=1)
+n_reports = data.values.sum() # total number of reports
+n_dreams = data.iloc[:,1:].values.sum() # all reports that include dream recall
+n_subjs = data.shape[0]
 
-total_reports = df.values.sum()
-total_drm_reports = df.iloc[:,1:].values.sum()
+# make a dataframe that has both count and proportion
+# for lucidity success based on different criterion.
+# note that this is a cumulative sum thing.
+
+# get the cumulative sum of LDs at each cutoff
+cumsum_cols = pd.np.roll(data.columns.sort_values(ascending=False),-1).tolist()
+cumsum_df = data[cumsum_cols].cumsum(axis=1)
 
 # don't use response of 1 ("Not at all") or "No recall"
 # flip it for interpretability
 DLQ_COLS = [ f'DLQ1_resp-{i}' for i in range(2,6) ]
 
-# get the proportion of all reports/attempts
-# and also proportion of only times were a dream was recalled
-# proportion_lds = [ cumsum_df[col].sum() / total_reports for col in cumsum_df.columns ]
-ld_prprtion_of_all  = cumsum_df[DLQ_COLS].apply(lambda srs: srs.sum() / total_reports, axis=0)
-ld_prprtion_of_drms = cumsum_df[DLQ_COLS].apply(lambda srs: srs.sum() / total_drm_reports, axis=0)
+index = pd.Index(DLQ_COLS,name='cutoff')
+columns = ['dreams_past_cutoff','subjs_past_cutoff']
 
-######### get proportion of success for each cutoff
-######### but now measured as proportion of subjs
-n_subjs = df.shape[0]
-# get a list of proportions of subjs that pass cutoff at each cutoff
-ld_prprtion_of_subjs = [ (cumsum_df[col] > 0).mean() for col in DLQ_COLS ]
+df = pd.DataFrame(columns=columns,index=index)
 
+df['dreams_past_cutoff'] = cumsum_df[DLQ_COLS].sum(axis=0)
+df['subjs_past_cutoff']  = cumsum_df[DLQ_COLS].astype(bool).sum(axis=0)
 
+df['proportion_reports'] = df['dreams_past_cutoff'] / n_reports
+df['proportion_dreams'] = df['dreams_past_cutoff'] / n_dreams
+df['proportion_subjs'] = df['subjs_past_cutoff'] / n_subjs
 
-
-
-data = dict(all=ld_prprtion_of_all,
-            recalled=ld_prprtion_of_drms,
-            subjs=ld_prprtion_of_subjs)
-markers = dict(all='s',recalled='o',subjs='^')
-labels = dict(all='Proportion of all attempts',
-              recalled='Proportion of recalled dreams',
-              subjs='Proportion of subjects')
-# append total to the labels
-totals = dict(all=total_reports,
-              recalled=total_drm_reports,
-              subjs=n_subjs)
-for key, lbl in labels.items():
-    labels[key] = f'{lbl} ($\mathit{{n}}$={totals[key]:d})'
+for col in df.columns:
+    if 'proportion' in col:
+        df[col] = df[col].round(2)
 
 
-#########  draw the plot  #########
+#########  stats  #########
+
+n_combos = 3 * len(list(combinations(DLQ_COLS,2)))
+
+dreams_past_cutoff = df['dreams_past_cutoff'].values
+n_observations = pd.np.repeat(n_dreams,dreams_past_cutoff.size)
+
+stats_df = pd.DataFrame(index=range(n_combos),
+    columns=['evaluation','cutoff_a','cutoff_b','zval','pval'])
+
+i = 0
+# compare for all subjs
+for cutoffA, cutoffB in combinations(DLQ_COLS,2):
+    a = df.loc[cutoffA,'subjs_past_cutoff']
+    b = df.loc[cutoffB,'subjs_past_cutoff']
+    z, p = proportions_ztest([a,b],[n_subjs,n_subjs])
+    stats_df.loc[i,'evaluation'] = 'subjs'
+    stats_df.loc[i,['cutoff_a','cutoff_b']] = [cutoffA,cutoffB]
+    stats_df.loc[i,['zval','pval']] = [z,p]
+    i += 1
+
+# compare for reports and nights
+# only thing different here is the n_observations
+cond_dict = dict(reports=n_reports,dreams=n_dreams)
+for label, n_obs in cond_dict.items():
+    for cutoffA, cutoffB in combinations(DLQ_COLS,2):
+        a = df.loc[cutoffA,'dreams_past_cutoff']
+        b = df.loc[cutoffB,'dreams_past_cutoff']
+        z, p = proportions_ztest([a,b],[n_obs,n_obs])
+        stats_df.loc[i,'evaluation'] = label
+        stats_df.loc[i,['cutoff_a','cutoff_b']] = [cutoffA,cutoffB]
+        stats_df.loc[i,['zval','pval']] = [z,p]
+        i += 1
+
+# round values while also changing output format to print full values
+stats_df['zval'] = stats_df['zval'].map(lambda x: '%.02f' % x)
+stats_df['pval'] = stats_df['pval'].map(lambda x: '%.04f' % x)
+
+
+# export data and results dataframes
+df_fname  = path.join(resdir,'induction_success-data.tsv')
+stats_fname = path.join(resdir,'induction_success-stats.tsv')
+df.to_csv(df_fname,index=True,sep='\t')
+stats_df.to_csv(stats_fname,index=False,sep='\t')
+
+
+
+#########  draw plot  #########
+
+markers = dict(reports='s',dreams='o',subjs='^')
+labels = dict(reports=f'Proportion of all reports ($\mathit{{n}}$={n_reports})',
+              dreams=f'Proportion of recalled dreams ($\mathit{{n}}$={n_dreams})',
+              subjs=f'Proportion of subjects ($\mathit{{n}}$={n_subjs})')
 
 fig, ax = plt.subplots(figsize=(5,5))
 
 # draw lines and points separately to have diff colored points
-for key, yvals in data.items():
-    ax.plot(range(2,6),yvals,
-        color='k',linestyle='-',linewidth=1,zorder=1)
-    ax.scatter(range(2,6),yvals,
-        color=[ myplt.dlqcolor(i) for i in range(2,6) ],
-        marker=markers[key],edgecolors='w',
-        s=150,zorder=2,linewidths=.5)
+for col in df.columns:
+    if 'proportion' in col:
+        key = col.split('_')[1]
+        yvals = df[col].values
+        ax.plot(range(2,6),yvals,
+            color='k',linestyle='-',linewidth=1,zorder=1)
+        ax.scatter(range(2,6),yvals,
+            color=[ myplt.dlqcolor(i) for i in range(2,6) ],
+            marker=markers[key],edgecolors='w',
+            s=150,zorder=2,linewidths=.5)
 
 # handle the xaxis
 ax.set_xticks(range(2,6))
@@ -107,12 +157,11 @@ legend_patches = [ matplotlib.lines.Line2D([],[],
                     color='gray',linestyle='none')
                 for key, label in labels.items() ]
 ax.legend(handles=legend_patches,loc='upper right',
-          title='Evaluation',
-          frameon=True,framealpha=1,edgecolor='k')
-
+          title='Evaluation',frameon=True,framealpha=1,edgecolor='k')
 
 plt.tight_layout()
 
-for ext in ['.png','.svg','.eps']:
-    plt.savefig(infname.replace('.tsv',f'-proportions{ext}'))
+for ext in ['png','svg','eps']:
+    plot_fname = path.join(resdir,f'induction_success-plot.{ext}')
+    plt.savefig(plot_fname)
 plt.close()
