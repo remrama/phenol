5169cc2278a1051eecce428e0791186726c8a409
remrama <mallett.remy@gmail.com>
2019-11-12 21:55:56 -0600

    break correlations script into separate resample and zscore

---
+++ correlations-resample.py
@@ -0,0 +1,94 @@
+"""
+Run all correlations (of interest) with DLQ1.
+
+Things to correlate with DLQ1:
+    - all things on morning survey about the dream
+      (e.g., bizarreness)
+    - positive and negative PANAS summary scores
+      (from each morning)
+    - dream control score (derived from DLQ/MUSK)
+
+There are multiple subject datapoints so can't just run a
+simple correlation because datapoints are not independent.
+
+So for each correlation, run a resampling procedure
+where for every iteration, one sample is drawn randomly
+per subject and a normal correlation is run across
+points (bc now there is only one datapoint per subject).
+For each dream characteristic, run N iterations.
+
+Fisher zscoring and pvalues come from correlations-zscore.py
+
+Export dataframe holding all the resampled correlations
+
+"""
+from os import path
+from json import load
+import tqdm
+import pandas as pd
+
+
+# load analysis parameters from configuration file
+with open('./config.json') as f:
+    p = load(f)
+    DATA_DIR = path.expanduser(p['data_directory'])
+    RES_DIR  = path.expanduser(p['results_directory'])
+    POS_PROBES = p['PANAS_positive_probes']
+    NEG_PROBES = p['PANAS_negative_probes']
+    CONTROL_PROBES = p['DLQ_control_probes']
+    N_RESAMPLES = p['n_correlation_resamples']
+    COLS2CORR = p['variables_to_correlate']
+
+
+#######  load and manipulate data  #######
+
+infname = path.join(DATA_DIR,'data-clean.tsv')
+df = pd.read_csv(infname,sep='\t')
+
+# drop all nights without recall
+df = df[df['dreamreport:1']!='No recall']
+
+# generate columns that require manipulations of the raw data
+panas_pos_cols = [ f'Affect:{x}' for x in POS_PROBES ]
+panas_neg_cols = [ f'Affect:{x}' for x in NEG_PROBES ]
+control_cols   = [ f'DLQ:{x}' for x in CONTROL_PROBES ]
+df['panas_pos']     = df[panas_pos_cols].mean(axis=1)
+df['panas_neg']     = df[panas_neg_cols].mean(axis=1)
+df['dream_control'] = df[control_cols].mean(axis=1)
+
+
+#######  analysis  #######
+
+# build dataframe to hold all the resampled correlations
+METRICS = ['slope','intercept','r']
+INDEX_NAMES = ['probe','resample']
+index_values = [COLS2CORR,range(N_RESAMPLES)]
+index = pd.MultiIndex.from_product(index_values,names=INDEX_NAMES)
+res_df = pd.DataFrame(columns=METRICS,index=index,dtype=float)
+
+# loop over each variable of interest and run N
+# correlations, resampling a random night from
+# each participant every time
+for col in tqdm.tqdm(COLS2CORR,desc='resampling correlations'):
+    for i in tqdm.trange(N_RESAMPLES,desc=col):
+        # sample one night from each subject, randomly
+        rsmpl_df = df.groupby('subj').apply(lambda df: df.sample(1))[['DLQ:1',col]]
+        # correlate col/var with DLQ1
+        r = rsmpl_df.corr(method='kendall').values[0,1]
+        # get the slope/intercept for later plotting
+        x = rsmpl_df[col].values
+        y = rsmpl_df['DLQ:1'].values
+        m, b = pd.np.polyfit(x,y,1)
+        # save to dataframe
+        res_df.loc[(col,i),METRICS] = [m,b,r]
+
+
+########  export  ########
+
+# round values while also changing output format to print full values
+for col in res_df.columns:
+    res_df[col] = res_df[col].map(lambda x: '%.02f' % x)
+res_fname = path.join(RES_DIR,'correlate_dlq1-data.tsv')
+res_df.to_csv(res_fname,index=True,sep='\t')
+
+print('\n') # clear last terminal line
---
+++ correlations-zscore.py
@@ -0,0 +1,75 @@
+"""
+Fisher zscore all r values from correlations-zscore.py
+and derived pvalues as as the proportion of fisher
+zscored R values that are above 0.
+
+Export dataframe holding the final stats
+and correlation parameters.
+
+Also resaves out the resampling file, now with
+just an additional column for the fisher zscores.
+"""
+from os import path
+from json import load
+import pandas as pd
+
+from statsmodels.stats.multitest import multipletests
+
+
+# load results directory from configuration file
+with open('./config.json') as f:
+    p = load(f)
+    RES_DIR  = path.expanduser(p['results_directory'])
+# choose params to make 95% confidence intervals
+CI_LO = .025
+CI_HI = .975
+
+
+#######  load and manipulate data  #######
+
+infname = path.join(RES_DIR,'correlate_dlq1-data.tsv')
+res_df = pd.read_csv(infname,sep='\t',index_col=['probe','resample'])
+
+
+#######  derive pvalues  #######
+
+# fisher zscore all r values
+def fisherz(x):
+    # arctanh can't handle -1 or 1 so this function accounts for that
+    if x in [-1,1]:
+        # multiply by x to 
+        x -= pd.np.sign(x) * .000001 # picked 6 decimal points bc that's the precision of other values
+    return pd.np.arctanh(x)
+res_df['rfishz'] = res_df['r'].map(fisherz)
+
+# initialize stats dataframe with the mean of each metric
+stats_df = res_df.groupby('probe').mean()
+stats_df.columns = [ f'{c}_mean' for c in stats_df.columns ]
+# add confidence intervals for r ans fisherz r values
+for col in ['r','rfishz']:
+    stats_df[f'{col}_cilo'] = res_df.groupby('probe')[col].quantile(CI_LO)
+    stats_df[f'{col}_cihi'] = res_df.groupby('probe')[col].quantile(CI_HI)
+
+# add pvalue based on fisher zscore
+stats_df['pval'] = res_df.groupby('probe').agg({'rfishz':lambda col: pd.np.mean(col<0)})
+
+# generate a pvalue accounting for multiple comparisons
+uncorrected_pvals = stats_df['pval'].values
+mc_results = multipletests(uncorrected_pvals,
+    alpha=0.05,method='fdr_bh',is_sorted=False,returnsorted=False)
+sig, corrp, alphacSidak, alphacBonf = mc_results
+stats_df['pval_corrected'] = corrp
+
+
+########  export dataframes  ########
+
+# round values while also changing output format to print full values
+for df in [stats_df,res_df]:
+    for col in df.columns:
+        fmt = '%.04f' if col == 'pval' else '%.02f'
+        df[col] = df[col].map(lambda x: fmt % x)
+
+stats_fname = path.join(RES_DIR,'correlate_dlq1-stats.tsv')
+stats_df.to_csv(stats_fname,index=True,sep='\t')
+
+res_df.to_csv(infname,index=True,sep='\t')
